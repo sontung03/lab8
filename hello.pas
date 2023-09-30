c576cfb05cb2e14e2a8fc7d75505e9c28cc4969e
local user <user@mil8a-118b-02.mmcs.sfedu.ru>
2023-09-30 09:42:00 +0300

    Initial commit

--- hello.pas
+++ hello.pas
@@ -1,3 +1,13 @@
-﻿begin
-  write('привет , мир!')
+﻿var hour: integer;
+begin
+  writeln('введите текущее иремя ( от 0 до 23):');
+  readln(hour);
+  if (hour >=4 ) and (hour <= 10) then
+    writeln('добрый утро, мир!')
+  else if (hour >=11) and (hour <= 16) then
+    writeln( 'Добрый день, мир!')
+  else if (hour >= 17) and (hour <= 22) then
+    writeln('Добрый вечер, мир!')
+  else
+    writeln('Доброй ночи, мир!');
 end.
